f4d8db0b3e74505f2f90146735c9c79ba11ea832
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-12-19 16:01:33 +0100

    Translate SQL into Python DB statements

--- logs.py
+++ logs.py
@@ -36,24 +36,18 @@ print("------TOP AUTHORS------")
 for author in top_authors:
   print("Author: {}, Total Views: {}".format(author[0], author[1]))
 
-# "select slug, count from articles join top_articles on path like CONCAT('%', slug)";
 
-
-# cursor.execute(sql.SQL("select slug, views from top_articles, articles WHERE top_articles.path LIKE (\'/article/%s\') order by top_articles.views ").format(sql.Identifier('articles.slug')))
-# top_articles = cursor.fetchall()
-# print(top_articles)
-
-
-
-
-# print(slugs)
-# for slug in slugs:
-#   cursor.execute("Select A.title from Articles as A, Log as L WHERE A.slug LIKE (\"_%s\")", (slug,))
-
-# print(results)
-
-# select path, count(path) from log group by path order by count(path) desc;
-
-
-
-# CREATE VIEW top_articles AS select path, count(path) from log group by path order by count(path) desc offset 1 limit 8
+#3rd Exercise
+print("3rd exercise")
+# Get all 200s
+create view OKs as select status, extract(doy from time) as day from log where status like '200%';
+#Count all 200s per day
+create view total_oks as select day, count(status) from OKs group by day;
+# Get all errors
+create view total_errors as select status, extract(doy from time) as day from log where status not like '200%';
+# Count all 404s per day 
+create view total_errors as select day, count(status) from errors group by day;
+# Global view with 200s and 404s per day
+create view global_view as select total_oks.day, total_oks.count as status200, total_errors.count as status404 from total_oks join total_errors on total_oks.day = total_errors.day;
+# Select Day with error percentage > 1%
+select day, cast(status404 as decimal) / cast(status200 as decimal)*100 as error_percentage from global_view where cast(status404 as decimal) / cast(status200 as decimal) > 0.01;
